f741c45173c644106bcbd907e3cbba3853631dcc
openyeoreum <yeoreum00128@gmail.com>
2023-12-15 04:17:14 +0000

    23121501

--- backend/b1_Api/b11_Domain/b111_Router/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = ['b1111_UserRouter',
-        #    'b1112_UserHistoryRouter',
-        #    'b1113_SubscriptionRouter',
-        #    'b1114_SubscriptionHistoryRouter',
-           'b1115_ProjectsStorageRouter',
-        #    'b1116_ProjectsStorageHistoryRouter',
-        #    'b1117_ProjectRouter',
-        #    'b1118_ProjectHistoryRouter'
-]
--- backend/b1_Api/b12_Main.py
+++ backend/b1_Api/b12_Main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 import sys
 sys.path.append("/yaas")
 
-from backend.b1_Api.b11_Domain.b111_Router import *
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from backend.b1_Api.b16_Routers import UserRouter
 
 app = FastAPI(debug=True)
 
@@ -19,11 +19,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(b1111_UserRouter.router)
+app.include_router(UserRouter)
 # app.include_router(b1112_UserHistoryRouter.router)
 # app.include_router(b1113_SubscriptionRouter.router)
 # app.include_router(b1114_SubscriptionHistoryRouter.router)
 app.include_router(b1115_ProjectsStorageRouter.router)
 # app.include_router(b1116_ProjectsStorageHistoryRouter.router)
-# app.include_router(b1117_ProjectRouter.router)
+app.include_router(b1116_ProjectsRouter.router)
 # app.include_router(b1118_ProjectHistoryRouter.router)

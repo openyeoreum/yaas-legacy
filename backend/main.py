c74523717ec609cee5ad3ae232afc83e2fc35b3f
openyeoreum <yeoreum00128@gmail.com>
2024-02-09 08:44:59 +0000

    24020901

--- backend/main.py
+++ backend/main.py
@@ -35,13 +35,26 @@ def SolutionUpdate(email, projectNameList, MessagesReview, BookGenre):
         
 ### Main2 : 콘텐츠 제작 ###
 
-def CreationUpdate(projectNameList, email, VoiceDataSet):
+def CreationUpdate(email, projectNameList, VoiceDataSet):
 
     if isinstance(projectNameList, list):
         for projectName in projectNameList:
             
             ### Step6 : 크리에이션이 오디오북 제작 ###
             CreationAudioBookUpdate(projectName, email, VoiceDataSet)
+            
+### YaaS : YaaS의 통합으로 'Solution', 'Creation' ###
+
+def YaaS(email, name, password, projectNameList, MessagesReview, BookGenre, VoiceDataSet, MainProcess):
+
+    if MainProcess == 'Solution':
+        AccountUpdate(email, name, password)
+        SolutionUpdate(email, projectNameList, MessagesReview, BookGenre)
+        
+    elif MainProcess == 'Creation':
+        AccountUpdate(email, name, password)
+        SolutionUpdate(email, projectNameList, MessagesReview, BookGenre)
+        CreationUpdate(projectNameList, email, VoiceDataSet)
 
 if __name__ == "__main__":
 
@@ -49,15 +62,11 @@ if __name__ == "__main__":
     email = "yeoreum00128@gmail.com"
     name = "yeoreum"
     password = "0128"
-    projectNameList = ['노인을위한나라는있다'] #, '데미안', '우리는행복을진단한다', '웹3.0메타버스', '살아서천국극락낙원에가는방법', '빨간머리앤', '나는선비로소이다', '나는노비로소이다', '카이스트명상수업']
+    projectNameList = ['노인을위한나라는있다'] # , '데미안', '우리는행복을진단한다', '웹3.0메타버스', '살아서천국극락낙원에가는방법', '빨간머리앤', '나는선비로소이다', '나는노비로소이다', '카이스트명상수업']
     MessagesReview = "on"
     BookGenre = "Auto" # 'Auto', '문학', '비문학', '아동', '시', '학술'
     VoiceDataSet = "TypeCastVoiceDataSet"
+    MainProcess = "Creation" # 'Solution', 'Creation'
     #########################################################################
 
-    ### Main1 : 솔루션 업데이트 ###
-    AccountUpdate(email, name, password)
-    SolutionUpdate(email, projectNameList, MessagesReview, BookGenre)
-    
-    # ### Main2 : 크리에이션 제작 ###
-    # CreationUpdate(projectNameList, email, VoiceDataSet)
+    YaaS(email, name, password, projectNameList, MessagesReview, BookGenre, VoiceDataSet, MainProcess)

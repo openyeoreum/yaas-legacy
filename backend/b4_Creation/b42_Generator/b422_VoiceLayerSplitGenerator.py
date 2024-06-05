597070c08136e5842b96ecc114c0ae52926b6549
openyeoreum <yeoreum00128@gmail.com>
2024-06-05 13:03:45 +0000

    24060501

--- backend/b4_Creation/b42_Generator/b422_VoiceLayerSplitGenerator.py
+++ backend/b4_Creation/b42_Generator/b422_VoiceLayerSplitGenerator.py
@@ -684,7 +684,7 @@ def ActorVoiceGen(projectName, email, voiceReverbe, tag, name, Chunk, EL_Chunk, 
                 else:
                     return name
                 ########## TypeCast API 요청 ##########
-            except KeyError:
+            except KeyError as e:
                 attempt += 1
                 print(f"[ KeyError 발생, 재시도 {attempt}/60: {e} ]")
                 time.sleep(60)  # 1분 대기 후 재시도

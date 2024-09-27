e0d6bae8b82b1ba37ec55e8d42cbe03969d2aafc
openyeoreum <yeoreum00128@gmail.com>
2024-09-27 15:40:52 +0000

    24092801

--- backend/b2_Solution/bm22_DataFrameUpdate.py
+++ backend/b2_Solution/bm22_DataFrameUpdate.py
@@ -164,6 +164,8 @@ def SolutionDataFrameUpdate(email, projectName, indexMode = "Define", messagesRe
     userStoragePath = "/yaas/storage/s1_Yeoreum/s12_UserStorage"
     DataFramePath = FindDataframeFilePaths(email, projectName, userStoragePath)
     RawDataSetPath = "/yaas/storage/s1_Yeoreum/s11_ModelFeedback/s111_RawDataSet/"
+    RawDataSetPath = f"/yaas/storage/s1_Yeoreum/s12_UserStorage/yeoreum_user/yeoreum_storage/{projectName}/{projectName}_dataset_file/"
+    
     ### existedDataFrameMode는 개발과정에서 지속적인 데이터베이스 포멧에 따라 필요, 프로덕트에서는 필요없음.
     existedDataFrameMode = "on" # <- 개발 후 off #
     

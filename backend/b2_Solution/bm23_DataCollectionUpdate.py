7724c12e182001cfd28fc8cb9d9b53919ee1fa05
openyeoreum <yeoreum00128@gmail.com>
2024-12-22 05:56:27 +0000

    24122201

---
+++ backend/b2_Solution/bm23_DataCollectionUpdate.py
@@ -0,0 +1,39 @@
+import os
+import json
+import unicodedata
+import re
+import math
+import sys
+sys.path.append("/yaas")
+
+from matplotlib import font_manager, rc
+import matplotlib.pyplot as plt
+from datetime import datetime
+from PIL import Image
+from reportlab.lib.pagesizes import portrait
+from reportlab.pdfgen import canvas
+
+########################################
+########################################
+##### SolutionDataCollectionUpdate #####
+########################################
+########################################
+
+def SolutionDataCollectionUpdate(email, projectName):
+
+
+    ###############################
+    ### 01_TargetDataCollection ###
+    ###############################
+
+
+    ##############################
+    ### 02_TrendDataCollection ###
+    ##############################
+    
+    
+    ###############################
+    ### 03_ScriptDataCollection ###
+    ###############################
+    
+    return None

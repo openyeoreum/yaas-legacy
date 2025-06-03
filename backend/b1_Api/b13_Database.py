e898953200be33f68bf614a1cb959ae3425c629a
openyeoreum <yeoreum00128@gmail.com>
2025-06-03 10:22:02 +0000

    25060301

--- backend/b1_Api/b13_Database.py
+++ backend/b1_Api/b13_Database.py
@@ -2,10 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from contextlib import contextmanager
 
-SQLALCHEMY_DATABASE_URL = "postgresql://yeoreum:0128@yaasdb:5432/yeoreum"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL)
+engine = create_engine("postgresql://yeoreum:0128@yaasdb:5432/yeoreum")
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
 @contextmanager

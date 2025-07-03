c5de39ddaa3717647d879a50d58f937d493efb83
openyeoreum <yeoreum00128@gmail.com>
2025-07-04 00:10:37 +0900

    25070401

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,23 @@
 <script>
-  let message;
-  fetch("http://127.0.0.1:8000/hello").then(response => {response.json().then(json => {message = json.message;});});
+  let question_list = []
+
+  function get_question_list() {
+    fetch("http://127.0.0.1:8000/api/question/list").then((response) => {
+      response.json().then((json) => {
+        question_list = json
+      })
+    })
+  }
+
+  get_question_list()
 </script>
 
-<h1>{message}</h1>
+<ul>
+  {#each question_list as question}
+    <li>
+      <h3>{question.subject}</h3>
+      <p>{question.answer}</p>
+      <p>Created at: {new Date(question.create_date).toLocaleString()}</p>
+    </li>
+  {/each}
+</ul>
